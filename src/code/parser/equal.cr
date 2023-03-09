e252413009aa757f32d689034d325840e6558c67
Dorian Marié <dorian@dorianmarie.fr>
2023-03-09 10:17:31 +0100

    equal

---
+++ src/code/parser/equal.cr
@@ -0,0 +1,97 @@
+class Code
+  class Parser
+    class Equal < Language
+      def rescue_class
+        Rescue
+      end
+
+      def equal_class
+        Equal
+      end
+
+      def whitespace
+        Whitespace
+      end
+
+      def whitespace?
+        whitespace.maybe
+      end
+
+      def equal
+        str("=")
+      end
+
+      def plus
+        str("+")
+      end
+
+      def minus
+        str("-")
+      end
+
+      def asterisk
+        str("*")
+      end
+
+      def slash
+        str("/")
+      end
+
+      def percent
+        str("%")
+      end
+
+      def greater
+        str(">")
+      end
+
+      def lesser
+        str("<")
+      end
+
+      def ampersand
+        str("&")
+      end
+
+      def pipe
+        str("|")
+      end
+
+      def caret
+        str("^")
+      end
+
+      def dot
+        str(".")
+      end
+
+      def operator
+        equal.ignore | (plus << equal.ignore) | (minus << equal.ignore) |
+          (asterisk << equal.ignore) | (slash << equal.ignore) |
+          (percent << equal.ignore) | (greater << greater << equal.ignore) |
+          (lesser << lesser << equal.ignore) | (ampersand << equal.ignore) |
+          (pipe << equal.ignore) | (caret << equal.ignore) |
+          (pipe << pipe << equal.ignore) |
+          (ampersand << ampersand << equal.ignore)
+      end
+
+      def root
+        (
+          rescue_class.aka(:left) <<
+            (
+              whitespace? <<
+                operator.aka(:operator) <<
+                whitespace? <<
+                equal_class.aka(:right)
+            ).maybe
+        ).aka(:equal).then do |output|
+          if output[:equal].not_nil!.fetch(:right, nil)
+            output
+          else
+            output[:equal].not_nil![:left].not_nil!
+          end
+        end
+      end
+    end
+  end
+end
--- src/code/parser/splat.cr
+++ src/code/parser/splat.cr
@@ -22,7 +22,7 @@ class Code
           ampersand.aka(:operator) <<
             whitespace? <<
             splat.aka(:right)
-        ).aka(:splat) | Rescue
+        ).aka(:splat) | Equal
       end
     end
   end

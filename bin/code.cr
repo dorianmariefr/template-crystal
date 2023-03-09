fbe6dc72d532efe1065fb418457e0023e138b87d
Dorian Marié <dorian@dorianmarie.fr>
2023-03-09 14:40:02 +0100

    add time elapsed and starting time to bin/code

--- bin/code.cr
+++ bin/code.cr
@@ -1,7 +1,12 @@
 #!/usr/bin/env crystal
 
+p Time.utc
+before = Time.utc
 require "../src/code/parser"
 
 input = ARGV.join(" ")
 input = File.read(input) if File.exists?(input)
 pp Code::Parser.parse(input)
+after = Time.utc
+
+puts after - before
